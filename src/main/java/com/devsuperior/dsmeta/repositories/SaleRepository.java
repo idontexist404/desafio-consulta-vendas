90889a9e54b77b9ef1139d768327c56005fe8701
Highlander Magno <highlander9200@gmail.com>
2024-07-19 04:42:51 -0300

    Add needed queries to SaleRepository

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,40 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("""
+           SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name, SUM(obj.amount)) 
+           FROM Sale obj 
+           WHERE obj.date BETWEEN :min AND :max 
+           GROUP BY obj.seller.name
+           """)
+    Page<SaleSummaryDTO> searchSummaries(LocalDate min, LocalDate max, Pageable pageable);
+
+    @Query(
+            value = """
+                SELECT obj 
+                FROM Sale obj 
+                JOIN FETCH obj.seller 
+                WHERE obj.date BETWEEN :min AND :max 
+                AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))
+                """,
+            countQuery = """
+                SELECT COUNT(obj) 
+                FROM Sale obj 
+                JOIN obj.seller 
+                WHERE obj.date BETWEEN :min AND :max 
+                AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))
+                """
+    )
+    Page<SaleReportDTO> searchReports(LocalDate min, LocalDate max, String name, Pageable pageable);
 }
